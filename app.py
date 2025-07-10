561a194938f23b4b84e0e75cc4ea6b92835021da
Nicole S <kotya@live.ca>
2025-07-09 20:42:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
+import requests
 import sys
 from PyQt5.QtCore import QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from dotenv import load_dotenv
+
+
+load_dotenv()
+api_key = os.getenv("WEATHER_API")
+CITY = 'New York'
+UNITS = 'metric' 
 
 # Create the application object
 app = QApplication(sys.argv)
@@ -9,6 +18,8 @@ app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('Cute Companion App')
 window.setGeometry(100, 100, 300, 300)  # x, y, width, height
+
+
 
 # Create the time label
 time_label = QLabel(parent=window)
@@ -25,9 +36,22 @@ timer.timeout.connect(update_time)
 timer.start(1000)
 update_time()  # Initial call
 
+def get_weather():
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={api_key}&units={UNITS}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        temp = data['main']['temp']
+        desc = data['weather'][0]['description']
+        return f"{int(temp)}°C, {desc.capitalize()}"
+    else:
+        return "Weather error"
 
-
-
+# Create and place weather label
+weather_label = QLabel(parent=window)
+weather_label.move(10, 10)
+weather_label.resize(200, 20)
+weather_label.setText(get_weather())
 
 
 # Show the window
